846d782b7a8e33f011451f0423d0c0e46983a378
Vladimir Pasev <vladigpasev@gmail.com>
2024-01-20 23:39:47 +0200

    Fixed some design stuff

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,9 +11,9 @@ function Dashboard() {
       <div className='pb-5'><h1 className='text-xl font-medium'>My Events</h1></div>
       <div className='w-full flex flex-grow items-center justify-center'>
         <div className='grid md:grid-cols-4 sm:grid-cols-3 supersmall:grid-cols-2 grid-cols-1 gap-5 w-fit'>
-          <div className='bg-white w-fit h-fit p-3 rounded'>
-            <div className='pb-2'>
-              <img src="/images/example.png" alt="Event Image" className='rounded' />
+        <div className='bg-white w-46 p-3 rounded overflow-hidden'>
+            <div className='pb-2 h-60'>
+              <img src="/images/example.png" alt="Event Image" className='w-full h-full object-cover object-center rounded' />
             </div>
             <div className='flex flex-col flex-grow'>
               <div className=" text-black text-base font-normal">Event name</div>
@@ -25,9 +25,9 @@ function Dashboard() {
               </div>
             </div>
           </div>
-          <div className='bg-white w-fit h-fit p-3 rounded'>
-            <div className='pb-2'>
-              <img src="/images/example.png" alt="Event Image" className='rounded' />
+          <div className='bg-white w-46 p-3 rounded overflow-hidden'>
+            <div className='pb-2 h-60'>
+              <img src="https://media.npr.org/assets/img/2022/11/04/gettyimages-1183414292-1-_slide-edff8c3fe6afcab5c6457e3c7bd011f5c1745161.jpg" alt="Event Image" className='w-full h-full object-cover object-center rounded' />
             </div>
             <div className='flex flex-col flex-grow'>
               <div className=" text-black text-base font-normal">Event name</div>
@@ -39,9 +39,9 @@ function Dashboard() {
               </div>
             </div>
           </div>
-          <div className='bg-white w-fit h-fit p-3 rounded'>
-            <div className='pb-2'>
-              <img src="/images/example.png" alt="Event Image" className='rounded' />
+          <div className='bg-white w-46 p-3 rounded overflow-hidden'>
+            <div className='pb-2 h-60'>
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqzelRvUGQxdhSn5G2MuMAkWQRfeVqfAAY2Q&usqp=CAU" alt="Event Image" className='w-full h-full object-cover object-center rounded' />
             </div>
             <div className='flex flex-col flex-grow'>
               <div className=" text-black text-base font-normal">Event name</div>
@@ -53,9 +53,9 @@ function Dashboard() {
               </div>
             </div>
           </div>
-          <div className='bg-white w-fit h-fit p-3 rounded'>
-            <div className='pb-2'>
-              <img src="/images/example.png" alt="Event Image" className='rounded' />
+          <div className='bg-white w-46 p-3 rounded overflow-hidden'>
+            <div className='pb-2 h-60'>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Chicago_Bulls_and_New_Jersey_Nets%2C_March_28%2C_1991.jpg/550px-Chicago_Bulls_and_New_Jersey_Nets%2C_March_28%2C_1991.jpg" alt="Event Image" className='w-full h-full object-cover object-center rounded' />
             </div>
             <div className='flex flex-col flex-grow'>
               <div className=" text-black text-base font-normal">Event name</div>
