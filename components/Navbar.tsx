f1b6316e85e8d8fe26aaa993540e8855005e886f
Vladimir Pasev <vladigpasev@gmail.com>
2024-02-19 11:15:13 +0200

    Navbars fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -57,39 +57,6 @@ function Navbar() {
                     </div>
                     <div className="flex-none">
                         <div className="dropdown dropdown-end">
-                            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                                <div className="indicator">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-                                    <span className="badge badge-xs badge-primary indicator-item"></span>
-                                </div>
-                            </div>
-                            <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
-                                <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-                                    <div className="px-4 py-5 sm:p-6 bg-gray-100">
-                                        <h3 className="text-lg leading-6 font-medium text-gray-900">Notifications</h3>
-                                        <p className="mt-1 max-w-2xl text-sm text-gray-500">You have 8 unread notifications</p>
-                                    </div>
-                                    <ul className="divide-y divide-gray-200">
-                                        <li className="px-4 py-4 bg-white hover:bg-gray-50">
-                                            <span className="block text-sm font-medium text-gray-700">Your order has been shipped</span>
-                                        </li>
-                                        <li className="px-4 py-4 bg-white hover:bg-gray-50">
-                                            <span className="block text-sm font-medium text-gray-700">Your account has been updated</span>
-                                        </li>
-                                    </ul>
-                                    <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                                        <button type="button" className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                                            View all notifications
-                                        </button>
-                                        <a href="#" className="text-sm font-medium text-blue-600 hover:text-blue-500">
-                                            Clear all notifications
-                                        </a>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                        <div className="dropdown dropdown-end">
                             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                                 <div className="w-10 rounded-full">
                                     <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
@@ -97,10 +64,10 @@ function Navbar() {
                             </div>
                             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                                 <li><a onClick={toggleMyPlan}>My plan</a></li>
-                                <li><a><form method="POST" action={manageAccount}>
-                                    <button type="submit">Billing settings</button>
-                                </form></a></li>
-                                <li><a><form action={handleLogout}><button type='submit'>Logout</button></form></a></li>
+                                <form action={manageAccount} className='w-full'>
+                                    <button type="submit" className='w-full'><li><a>Billing settings</a></li></button>
+                                </form>
+                                <form action={handleLogout} className='w-full'><button type='submit' className='w-full'> <li><a>Logout</a></li></button></form>
                             </ul>
                         </div>
                     </div>
@@ -127,8 +94,8 @@ function Navbar() {
                                 <div className={`w-[93.28px] ${isActive('/dashboard') ? 'text-blue-800' : 'text-white'} text-sm font-medium`}>Dashboard</div>
                             </div>
                         </Link>
-                        <Link href="/dashboard/invoices">
-                            <div className={`w-full h-14 rounded border flex items-center gap-4 cursor-pointer ${isActive('/dashboard/invoices') ? 'bg-white border-gray-100 hover:bg-gray-100' : 'border-transparent hover:bg-white hover:bg-opacity-10'}`}>
+                        <form action={manageAccount} className={`w-full`}><button type='submit' className='w-full'>
+                            <div className={`w-full h-14 rounded border flex items-center cursor-pointer ${isActive('/dashboard/invoices') ? 'bg-white border-gray-100 hover:bg-gray-100' : 'border-transparent hover:bg-white hover:bg-opacity-10'}`}>
                                 <div className="flex justify-center items-center pl-2">
                                     <div className={`rounded-full p-2 ${isActive('/dashboard/invoices') ? 'bg-white bg-opacity-50 text-blue-800' : 'bg-white bg-opacity-50 text-white'}`}>
                                         <InvoicesSvg />
@@ -136,17 +103,7 @@ function Navbar() {
                                 </div>
                                 <div className={`w-[93.28px] ${isActive('/dashboard/invoices') ? 'text-blue-800' : 'text-white'} text-sm font-medium`}>Invoices</div>
                             </div>
-                        </Link>
-                        <Link href="/dashboard/settings">
-                            <div className={`w-full h-14 rounded border flex items-center gap-4 cursor-pointer ${isActive('/dashboard/settings') ? 'bg-white border-gray-100 hover:bg-gray-100' : 'border-transparent hover:bg-white hover:bg-opacity-10'}`}>
-                                <div className="flex justify-center items-center pl-2">
-                                    <div className={`rounded-full p-2 ${isActive('/dashboard/settings') ? 'bg-white bg-opacity-50 text-blue-800' : 'bg-white bg-opacity-50 text-white'}`}>
-                                        <SettingsSvg />
-                                    </div>
-                                </div>
-                                <div className={`w-[93.28px] ${isActive('/dashboard/settings') ? 'text-blue-800' : 'text-white'} text-sm font-medium`}>Settings</div>
-                            </div>
-                        </Link>
+                        </button></form>
                     </div>
                     <div className='w-full flex flex-col gap-7 mb-5'>
                         <form action={handleLogout}>
